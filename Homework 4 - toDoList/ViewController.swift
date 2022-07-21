7ecd812c23b5d85c0dc50c3dcf40be45426066d6
olvr1812 <olvr1812@gmail.com>
2022-07-21 10:46:22 +0300

    Initial Commit

---
+++ Homework 4 - toDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 4 - toDoList
+//
+//  Created by Оливер Салихов on 21.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
